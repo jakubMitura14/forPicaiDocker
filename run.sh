de59ad7453c787147e6e247e7851daa5f66d8706
Jakub Mitura <jakub.mitura14@gmail.com>
2022-06-29 06:07:44 +0200

    debugging start sh
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+echo "starttttt a"
 
 set -ex
+echo "starttttt b"
 
 script_dir=$(cd $(dirname $0) || exit 1; pwd)
+
+echo "starttttt c"
 
 ################################################################################
 # Set up headless environment
 source $script_dir/start-xorg.sh
+
+echo "starttttt d"
 
 #/usr/bin/x11vnc -forever -rfbport $VNCPORT -display :10 -shared -bg -auth none -nopw
 #sleep 1
@@ -20,9 +26,12 @@ source $script_dir/start-xorg.sh
 
 ################################################################################
 # Set jupyter terminal to bash (better auto-complete, etc. than default sh)
+echo "starttttt e"
 
 export SHELL=/bin/bash
+echo "starttttt f"
 
 ################################################################################
 # this needs to be last
 exec "$@"
+echo "starttttt g"
