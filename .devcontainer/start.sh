489fed8e2a8278cb13b3833ebf3a4163d5d1ddf0
Jakub Mitura <jakub.mitura14@gmail.com>
2022-10-18 21:23:31 +0200

    up
    Signed-off-by: Jakub Mitura <jakub.mitura14@gmail.com>

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,5 +1,6 @@
 echo "staaarting "
 # docker run --init --gpus all --ipc host --privileged --net host -p 8888:8888 -p49053:49053 --restart unless-stopped -v /mnt/disks/sde:/home/sliceruser/data -v /mnt/disks/sdb:/home/sliceruser/dataOld -it  slicerpicai:latest bash
+git config --global --add safe.directory /home/sliceruser/locTemp/piCaiCode
 cd /home/sliceruser/locTemp/piCaiCode
 git pull
 python3.8 /home/sliceruser/locTemp/piCaiCode/Three_chan_baseline_hyperParam.py
